bf0eb8e380bb3be4d820a62ed193331b5e6da4ae
liyang <995720636@qq.com>
2016-03-17 17:21:44 +0800

    Initial Commit

---
+++ LYTouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYTouchDemo
+//
+//  Created by liyang on 16/3/17.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYTouchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYTouchDemo
+//
+//  Created by liyang on 16/3/17.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
